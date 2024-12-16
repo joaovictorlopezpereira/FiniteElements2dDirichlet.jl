ac0752f22f89582729b6f9a556e6dbd833e3e80a
João Victor Lopez Pereira <142049069+joaovictorlopezpereira@users.noreply.github.com>
2024-12-16 14:47:08 -0300

    granting access to other vectors

--- src/FiniteElements2dDirichlet.jl
+++ src/FiniteElements2dDirichlet.jl
@@ -10,39 +10,9 @@ using SparseArrays
 using LinearAlgebra
 
 
-# Phi function-vector
-phi = [
-  (xi1, xi2) -> (1 - xi1) * (1 - xi2) * (1 / 4);
-  (xi1, xi2) -> (1 + xi1) * (1 - xi2) * (1 / 4);
-  (xi1, xi2) -> (1 + xi1) * (1 + xi2) * (1 / 4);
-  (xi1, xi2) -> (1 - xi1) * (1 + xi2) * (1 / 4)
-]
+include("phi_dphi.jl") # phi, d_xi_phi
 
-# Derivative of the phi function-vector
-d_xi_phi = [
-  [((xi1, xi2) -> (-1 / 4) * (1 - xi2)),
-   ((xi1, xi2) -> ( 1 / 4) * (1 - xi2)),
-   ((xi1, xi2) -> ( 1 / 4) * (1 + xi2)),
-   ((xi1, xi2) -> (-1 / 4) * (1 + xi2))],
-  [((xi1, xi2) -> (-1 / 4) * (1 - xi1)),
-   ((xi1, xi2) -> (-1 / 4) * (1 + xi1)),
-   ((xi1, xi2) -> ( 1 / 4) * (1 + xi1)),
-   ((xi1, xi2) -> ( 1 / 4) * (1 - xi1))]
-]
-
-# X function-vector
-x = [
-  (xi1, xi2, Xs) -> sum(Xs[k] * phi[k](xi1, xi2) for k in 1:4),
-  (xi1, xi2, Ys) -> sum(Ys[k] * phi[k](xi1, xi2) for k in 1:4)
-]
-
-# Derivative of the x function-vector
-d_xi_x = [
-  [(xi1, xi2, coords) -> sum(coords[k] * d_xi_phi[1][k](xi1, xi2) for k in 1:4),
-   (xi1, xi2, coords) -> sum(coords[k] * d_xi_phi[2][k](xi1, xi2) for k in 1:4)],
-  [(xi1, xi2, coords) -> sum(coords[k] * d_xi_phi[1][k](xi1, xi2) for k in 1:4),
-   (xi1, xi2, coords) -> sum(coords[k] * d_xi_phi[2][k](xi1, xi2) for k in 1:4)]
-]
+include("x_dx.jl") # x, d_xi_x
 
 include("LG.jl") # init_LG_matrix
 
---
+++ src/phi_dphi.jl
@@ -0,0 +1,20 @@
+
+# Phi function-vector
+phi = [
+  (xi1, xi2) -> (1 - xi1) * (1 - xi2) * (1 / 4);
+  (xi1, xi2) -> (1 + xi1) * (1 - xi2) * (1 / 4);
+  (xi1, xi2) -> (1 + xi1) * (1 + xi2) * (1 / 4);
+  (xi1, xi2) -> (1 - xi1) * (1 + xi2) * (1 / 4)
+]
+
+# Derivative of the phi function-vector
+d_xi_phi = [
+  [((xi1, xi2) -> (-1 / 4) * (1 - xi2)),
+   ((xi1, xi2) -> ( 1 / 4) * (1 - xi2)),
+   ((xi1, xi2) -> ( 1 / 4) * (1 + xi2)),
+   ((xi1, xi2) -> (-1 / 4) * (1 + xi2))],
+  [((xi1, xi2) -> (-1 / 4) * (1 - xi1)),
+   ((xi1, xi2) -> (-1 / 4) * (1 + xi1)),
+   ((xi1, xi2) -> ( 1 / 4) * (1 + xi1)),
+   ((xi1, xi2) -> ( 1 / 4) * (1 - xi1))]
+]
